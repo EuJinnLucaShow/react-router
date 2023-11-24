4d82d7ebdcab9dd002a33e418b0f10d0c162125c
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-11-24 23:26:55 +0200

    NavLink className

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,9 +4,15 @@ export default function Layout() {
   return (
     <>
       <header style={{display: 'flex', justifyContent: 'space-evenly'}}>
-        <NavLink to='/home'>Home</NavLink>
-        <NavLink to='/catalog'>Catalog</NavLink>
-        <NavLink to='/profile'>Profile</NavLink>
+        <NavLink to='/home' className={({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "active" : ""
+  }>Home</NavLink>
+        <NavLink to='/catalog' className={({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "active" : ""
+  }>Catalog</NavLink>
+        <NavLink to='/profile' className={({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "active" : ""
+  }>Profile</NavLink>
       </header>
       <main>
       <Outlet></Outlet>
